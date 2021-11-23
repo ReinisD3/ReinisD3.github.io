44a1d86745ac62a231db87298b5375f39371e5bf
Reinis <reinis.detlavs@gmail.com>
2021-11-23 13:24:25 +0200

    Counter not Counting

---
+++ index.js
@@ -0,0 +1,19 @@
+// const button = document.getElementById("clickMe");
+// let count = 0;
+//
+//
+// button.addEventListener('click', function () {
+//     count += 1;
+//     button.innerHTML = "Clicked me: " + count;
+// });
+
+let clicks = 0;
+function click() {
+    clicks += 1;
+    document.getElementById("clickMe").innerHTML = clicks;
+};
+
+
+
+
+
